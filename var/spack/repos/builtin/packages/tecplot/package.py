48ae86d753b705deee23db5233923ec99ce115f5
Ben Kirk <benkirk@ucar.edu>
2022-12-18 10:15:29 -0700

    adapt tecplot package.py to use FSL downloads

--- var/spack/repos/builtin/packages/tecplot/package.py
+++ var/spack/repos/builtin/packages/tecplot/package.py
@@ -5,7 +5,7 @@
 
 import os
 
-from spack.package import *
+from spack import *
 
 
 class Tecplot(Package):
@@ -15,19 +15,25 @@ class Tecplot(Package):
     by post-processing charge density data."""
 
     homepage = "https://www.tecplot.com/"
-    manual_download = True
+    #manual_download = True
 
-    version("2017r1", "06a8057d33a519607720d4c621cd3f50", expand=False)
-    version("2018r2", "d3cf54a7555e0259b7ba0d82fef23bc3", expand=False)
+    version('2021r2', '768264ec76111613ca753226fce2d08c85ae4e4c6fdc8760cbc13a0c00678dd9', expand=False)
+    version('2020r2', '8cc121ab46f86573fdf1c4b505ee09aa7c9035b1a12ae0a2bec283d9cb5c29eb', expand=False)
+    version('2018r2', '768264ec76111613ca753226fce2d08c85ae4e4c6fdc8760cbc13a0c00678dd9', expand=False)
 
     def url_for_version(self, version):
-        return "file://{0}/tecplot360ex{1}_linux64.sh".format(os.getcwd(), version)
+        #return "file://{0}/tecplot360ex{1}_linux64.sh".format(os.getcwd(), version)
+
+        # sample: /aerolab/admin/software/dist/tecplot/tecplot360ex_2021_r2/tecplot360ex2021r2_linux64.sh
+        uversion = '{}'.format(version).replace('r', '_r')
+        return  "file:///aerolab/admin/software/dist/tecplot/tecplot360ex_{0}/tecplot360ex{1}_linux64.sh".format(uversion, version)
 
     def install(self, spec, prefix):
-        makefile = FileFilter(self.stage.archive_file)
-        makefile.filter("interactive=TRUE", "interactive=FALSE")
-        makefile.filter("cpack_skip_license=FALSE", "cpack_skip_license=TRUE")
+        #BSK: skip this, seems to corrupt the emebedded .tgz.  We can accomplish the same with command-line args.
+        #makefile = FileFilter(self.stage.archive_file)
+        #makefile.filter('interactive=TRUE', 'interactive=FALSE')
+        #makefile.filter('cpack_skip_license=FALSE', 'cpack_skip_license=TRUE')
 
         set_executable(self.stage.archive_file)
         installer = Executable(self.stage.archive_file)
-        installer("--prefix=%s" % prefix)
+        installer('--skip-license', '--exclude-chorus', '--prefix=%s' % prefix)
