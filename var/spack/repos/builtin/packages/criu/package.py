39d46a79177ea35ffc03a95804acf74b82494c9f
Ben Kirk <benkirk@ucar.edu>
2023-01-10 10:52:05 -0700

    CRIU package development

---
+++ var/spack/repos/builtin/packages/criu/package.py
@@ -0,0 +1,50 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Criu(MakefilePackage):
+    """CRIU: Checkpoint/Restore In Userspace can freeze a running
+    container, or an individual application, and checkpoint its state
+    to disk. The data saved can be used to restore the application and
+    run it exactly as it was during the time of the freeze.  """
+
+    homepage = "http://criu.org/"
+    url = "http://github.com/checkpoint-restore/criu/archive/v3.17.1/criu-3.17.1.tar.gz"
+
+    maintainers = ["benkirk"]
+
+    conflicts("platform=darwin", msg="CRIU requires Linux")
+    conflicts("platform=windows", msg="CRIU requires Linux")
+
+    version("3.17.1", sha256="f90fe2323ed1b84f273dc41dde1a38dd424157a57f713d1ba39094e70f90eca6")
+
+    variant("doc", default=True, description="Build documentation")
+    variant("tests", default=False, description="Build testsuite")
+
+    # ref: https://criu.org/Installation
+    depends_on("pkgconfig", type="build")
+
+    depends_on("binutils")
+    depends_on("gnutls")
+    depends_on("iproute2")
+    depends_on("libbsd")
+    depends_on("libcap")
+    depends_on("libnet")
+    depends_on("protobuf")
+    depends_on("protobuf-c")
+    depends_on("py-ipaddress")
+    depends_on("py-protobuf")
+
+    depends_on("asciidoc", when="+doc")
+    depends_on("xmlto", when="+doc")
+
+    depends_on("libaio", when="+tests")
+    depends_on("py-future", when="+tests")
+
+    def install(self, spec, prefix):
+        make()
+        make("PREFIX={0}".format(prefix), "install")
