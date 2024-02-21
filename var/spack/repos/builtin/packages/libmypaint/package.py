c54e360bf4443388ca5babe626d8e803f8505c24
Benjamin Kirk <benjamin.s.kirk@gmail.com>
2024-02-21 05:11:54 +0000

    libmypaint: add gettext dependency uncovered on a minimal host

--- var/spack/repos/builtin/packages/libmypaint/package.py
+++ var/spack/repos/builtin/packages/libmypaint/package.py
@@ -31,6 +31,7 @@ class Libmypaint(AutotoolsPackage):
     depends_on("perl-xml-parser")
     depends_on("babl", when="+gegl")
     depends_on("gegl", when="+gegl")
+    depends_on("gettext")
     depends_on("gobject-introspection", when="+introspection")
     depends_on("glib", when="+introspection")
 
