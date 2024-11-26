3ea1848795054bff6992ec27cf42e76899d96835
Benjamin Kirk <benkirk@ucar.edu>
2024-11-26 08:26:46 -0700

    change download URL

--- var/spack/repos/builtin/packages/gdk-pixbuf/package.py
+++ var/spack/repos/builtin/packages/gdk-pixbuf/package.py
@@ -12,7 +12,7 @@ class GdkPixbuf(MesonPackage):
     GTK+ 2 but it was split off into a separate package in preparation for the change to GTK+ 3."""
 
     homepage = "https://gitlab.gnome.org/GNOME/gdk-pixbuf"
-    url = "https://ftp.acc.umu.se/pub/gnome/sources/gdk-pixbuf/2.40/gdk-pixbuf-2.40.0.tar.xz"
+    url = "https://download.gnome.org/sources/gdk-pixbuf/2.42/gdk-pixbuf-2.42.12.tar.xz"
     git = "https://gitlab.gnome.org/GNOME/gdk-pixbuf"
     list_url = "https://ftp.acc.umu.se/pub/gnome/sources/gdk-pixbuf/"
     list_depth = 1
